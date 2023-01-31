6dde0360dd0c215d168ec5135b0ac8df57b79680
Szymon Smyk <szymon.smyk00@gmail.com>
2023-01-31 17:04:27 +0100

    added interval

--- main.js
+++ main.js
@@ -14,16 +14,19 @@ tiles.addTo(ISSmap)
 
 
 const api_url = 'https://api.wheretheiss.at/v1/satellites/25544'
+let firstTime = true
 async function getISS(){
     const response = await fetch(api_url)
     const data = await response.json()
     const {latitude, longitude} = data
 
     marker.setLatLng([latitude, longitude])
-
+    if (firstTime) {
+        ISSmap.setView([latitude, longitude], 2)
+        firstTime = false
+    }
     document.getElementById("lat").textContent = latitude.toFixed(5)
     document.getElementById("lon").textContent = longitude.toFixed(5)
 }
-
-
 getISS()
+setInterval(getISS, 1000)
