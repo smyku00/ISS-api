2ee0ad39a87ca2fb07ed0de1f2ce35e80da16708
Szymon Smyk <szymon.smyk00@gmail.com>
2023-01-31 16:54:18 +0100

    fixed map

--- main.js
+++ main.js
@@ -1,17 +1,15 @@
 //Making map and tiles
 const ISSmap = L.map('ISSmap').setView([0, 0], 1)
-const marker = L.marker([0, 0], {icon: issIcon}).addTo(ISSmap)
-const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-const tileURL = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-const tiles = L.tileLayer(tileURL, {attribution})
-tiles.addTo(ISSmap)
-
-//Making marker with icon
 const issIcon = L.icon({
     iconUrl: 'ISS.png',
     iconSize: [20, 32],
     iconAnchor: [25, 16],
 })
+const marker = L.marker([0, 0], {icon: issIcon}).addTo(ISSmap)
+const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+const tileURL = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+const tiles = L.tileLayer(tileURL, {attribution})
+tiles.addTo(ISSmap)
 
 
 
